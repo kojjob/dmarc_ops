2d4770f7c337dfaa6cb3f50d56cd3c273ab920cb
Kojo <kodeon6@gmail.com>
2026-01-15 01:58:30 +0000

    test: fix tests for new landing page and auth layout
    - Update PageControllerTest to match DMARC Ops landing page content
    - Restore auth navigation in root layout for tests
    - Configure Swoosh test adapter

--- test/dmarc_ops_web/controllers/page_controller_test.exs
+++ test/dmarc_ops_web/controllers/page_controller_test.exs
@@ -3,6 +3,7 @@ defmodule DmarcOpsWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Email Authentication"
+    assert html_response(conn, 200) =~ "Command Center"
   end
 end
